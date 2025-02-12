ef343058f1b514ed84aa13c6b0fab2d1cd9e7e42
sumeyye-kartal <smyyekartal@outlook.com>
2025-02-12 23:07:57 +0300

    done

--- src/main/java/com/workintech/sqlrelations/repository/DoctorRepository.java
+++ src/main/java/com/workintech/sqlrelations/repository/DoctorRepository.java
@@ -4,4 +4,5 @@ import com.workintech.sqlrelations.entity.Doctor;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface DoctorRepository extends JpaRepository<Doctor, Long> {
+
 }
